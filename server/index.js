17b2e1ac3aa4dc572f44848b5abc2823bf1bbb79
Chaim <kai.balsamo.abat@gmail.com>
2024-08-16 19:19:10 +0800

    added implementation of API end points for the chat and messages.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors"); // allow us to communicate in the frontend
 const mongoose = require("mongoose");
 const userRoute = require("./route/userRoute");
+const chatRoute = require("./route/chatRoute");
+const messageRoute = require("./route/messageRoute");
 require("dotenv").config();
 
 app.use(express.json());
@@ -14,6 +16,8 @@ const mongoDbURI = process.env.MONGODB_URI;
 app.use(cors());
 
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to our chat app APIs");
